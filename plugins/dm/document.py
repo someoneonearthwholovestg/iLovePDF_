b23384c9c9402192ac903bd1900e8e5acc1da77e
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-08-03 07:43:33 +0530

    Update document.py

--- plugins/dm/document.py
+++ plugins/dm/document.py
@@ -34,9 +34,9 @@ from configs.images import WELCOME_PIC, BANNED_PIC, BIG_FILE, PDF_THUMBNAIL
 
 try:
     import aspose.words as word
-    wordSupport == True
+    wordSupport = True
 except Exception:
-    wordSupport == False
+    wordSupport = False
 
 #--------------->
 #--------> convertAPI INSTANCE
